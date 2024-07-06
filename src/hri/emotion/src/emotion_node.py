f5f953100c8b89fb95709095f042d3c70b656a55
GiovanniFalcone <giova.falcone@studenti.unina.it>
2024-07-06 18:36:12 +0200

    rimossa camera per test

--- src/hri/emotion/src/emotion_node.py
+++ src/hri/emotion/src/emotion_node.py
@@ -8,6 +8,8 @@ from cv_bridge import CvBridge, CvBridgeError
 from deepface import DeepFace
 import cv2
 
+import time
+
 class EmotionModule:
     def __init__(self):
         self.last_emotion = ''
@@ -15,11 +17,26 @@ class EmotionModule:
         self.bridge = CvBridge()
         # Load face cascade classifier
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        #rospy.Subscriber('/person_detected', Image, self.detect_emotion_callback_2)
         rospy.Subscriber("/usb_cam/image_raw", Image, self.detect_emotion_callback, queue_size=1)
         self.emotion_publisher = rospy.Publisher('emotion', String, queue_size=10)
 
+    
     def detect_emotion_callback(self, ros_image):
+        frame = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
+        # Convert frame to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        try:
+            start_time = time.time()
+            result = DeepFace.analyze(frame, actions=['emotion'])
+            end_time = time.time()
+            print("Time to extract face: ",end_time-start_time)
+            dominant_emotion = result[0]['dominant_emotion']
+            self.handle_emotion(result)
+        except Exception as e:
+            pass
+
+
+    def detect_emotion_callback_2(self, ros_image):
         try:
             frame = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
             # Convert frame to grayscale
@@ -28,8 +45,10 @@ class EmotionModule:
             faces = self.face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
             for (x, y, w, h) in faces:
                 try:
-                    
+                    start_time = time.time()
                     result = DeepFace.analyze(frame, actions=['emotion'])
+                    end_time = time.time()
+                    print("Time to extract face: ",end_time-start_time)
                     dominant_emotion = result[0]['dominant_emotion']
                     self.handle_emotion(result)
                     #cv2.putText(frame, dominant_emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
@@ -48,15 +67,9 @@ class EmotionModule:
     def handle_emotion(self, emotion):
         dominant_emotion = emotion[0]['dominant_emotion']
         score_dominant_emotion = emotion[0]["emotion"][dominant_emotion]
-        if score_dominant_emotion > 50:
-            print("Sending ", dominant_emotion)
-            self.emotion_publisher.publish(dominant_emotion)
-            """if self.last_emotion == '':
-                self.last_emotion = dominant_emotion
-                print("Sending ", dominant_emotion)
-            elif self.last_emotion != dominant_emotion:
-                print("Sending ", dominant_emotion)
-                self.last_emotion = dominant_emotion"""
+        #if score_dominant_emotion > 50:
+        print("Sending ", dominant_emotion)
+        self.emotion_publisher.publish(dominant_emotion)
 
     def detect_emotion_callback_2(self, ros_image):
         if self.person_found:
